f7dd05052c4ee2c858fac979577331c6446c8b20
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2019-04-20 21:37:14 +0300

    Integration of salvattore layout

--- js/site.js
+++ js/site.js
@@ -8,34 +8,106 @@ $(window).scroll(function () {
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function () {
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1000, 'easeOutSine');
-        event.preventDefault();
-    });
-});
+// $(function () {
+//     $('a.page-scroll').bind('click', function (event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top
+//         }, 1000, 'easeOutSine');
+//         event.preventDefault();
+//     });
+// });
 
 $(function () {
 
-    var createGrid = function (template, data) {
-
-        var parent = template.parent();
-        var clones = [];
-        template.remove();
-
-        for (var i = 0, n = data.length; i < n; i++) {
-            var clone = template.clone();
-            clone.find('img').attr('src', data[i].thumb);
-            clone.find('a').attr('href', data[i].src);
-            clones.push(clone);
+    var balanceGridColumns = function ($grid) {
+        var $columns = $grid.find('.column');
+        if ($columns.length < 2) {
+            return;
         }
+        var maxIterations = 10;
+        while (maxIterations > 0) {
+            maxIterations--;
 
-        parent.append(clones);
+            var precision = 10;
+            var $lowestColumn = null;
+            var $highestColumn = null;
+            var lowColumnHeight = Number.MAX_VALUE;
+            var highColumnHeight = 0;
+            var hasMoreThanOneItem = false;
+
+            $columns.each(function () {
+                var $column = $(this);
+                if ($column.children().length > 1) {
+                    hasMoreThanOneItem = true;
+                }
+                var columnHeight = $column.outerHeight();
+                if (lowColumnHeight > columnHeight) {
+                    lowColumnHeight = columnHeight;
+                    $lowestColumn = $column;
+                }
+                if (highColumnHeight < columnHeight) {
+                    highColumnHeight = columnHeight;
+                    $highestColumn = $column;
+                }
+            });
+            if (!hasMoreThanOneItem ||
+                !$lowestColumn || !$highestColumn ||
+                !$lowestColumn[0] || !$highestColumn[0] ||
+                $lowestColumn[0] === $highestColumn[0]) {
+                return;
+            }
+            var compensatingHeight = highColumnHeight - lowColumnHeight;
+            if (compensatingHeight < precision) {
+                return;
+            }
+            var $compensatingItem = $highestColumn.children().eq(-1);
+            if ($compensatingItem.outerHeight() < compensatingHeight - precision) {
+                $lowestColumn.append($compensatingItem);
+            } else {
+                return;
+            }
+        }
     };
 
-    createGrid($('#stairs-section').find('.grid-template'), site.data.stairs);
-    createGrid($('#doors-section').find('.grid-template'), site.data.doors);
+    var balanceGridTimeout = 0;
+    var balanceGrids = function () {
+        clearTimeout(balanceGridTimeout);
+        balanceGridTimeout = setTimeout(function () {
+            $('.grid').each(function () {
+                balanceGridColumns($(this));
+            });
+        }, 40); // Salvattore has 30ms update delay
+    };
+
+    $('.grid').each(function () {
+
+        var $grid = $(this);
+        var n = $grid.data('items');
+        var template = $(this).find('script[type=template]').text();
+
+        var loadedImages = n;
+        var imgLoadHandler = function () {
+            loadedImages--;
+            if (loadedImages <= 0) {
+                var $grid = $(this).closest('.grid');
+                salvattore.recreateColumns($grid.get(0));
+                balanceGrids();
+            }
+        };
+
+        var items = [];
+        for (var i = 0; i < n; i++) {
+            var item = template.replace(/\{index\}/g, i < 10 ? '0' + i.toString() : i.toString());
+            var $item = $(item);
+            $item.find('img').on('load', imgLoadHandler);
+            items.push($item.get(0));
+        }
+
+        salvattore.appendElements($grid.get(0), items);
+    });
+
+    $(window).resize(function () {
+        balanceGrids();
+    });
 });
